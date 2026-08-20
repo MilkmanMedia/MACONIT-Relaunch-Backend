f3c604e97ba5d2b9f7c5427613c2191775ed923d
Daniel Köntös <daniel.koentoes@maconit.de>
2026-08-20 15:22:30 +0200

    Add new migrations for maintenance mode

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260820_121006_initial from './20260820_121006_initial';
+import * as migration_20260820_132159_add_maintenance_mode from './20260820_132159_add_maintenance_mode';
 
 export const migrations = [
   {
     up: migration_20260820_121006_initial.up,
     down: migration_20260820_121006_initial.down,
-    name: '20260820_121006_initial'
+    name: '20260820_121006_initial',
+  },
+  {
+    up: migration_20260820_132159_add_maintenance_mode.up,
+    down: migration_20260820_132159_add_maintenance_mode.down,
+    name: '20260820_132159_add_maintenance_mode'
   },
 ];
